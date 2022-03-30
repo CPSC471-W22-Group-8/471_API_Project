b3e14f953ac6f22380aff93defeb7ab27ccfddc0
Anish Pokhrel <anishpokhrel2002@gmail.com>
2022-03-30 03:32:33 -0600

    app setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
