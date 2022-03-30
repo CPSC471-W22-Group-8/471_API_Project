192a2af00b4da1e4b5bb0eb1e2fee7036cfaba93
Anish Pokhrel <anishpokhrel2002@gmail.com>
2022-03-30 17:03:10 -0600

    Added routing for table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
 //import { observer } from 'mobx-react';  // mobx-react not working
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Login';
 import Test from './Test';
 import Test2 from './Test2';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Table from './Table';
+
 
 // routing
 function App() {
   return (
     <BrowserRouter>
-    <div className='app'>
+    <div className='app' >
       <Routes>
-      <Route exact path='/' element={<Login />} /> 
-      <Route path='/test' element={<Test />} />
-      <Route path='/test2' element={<Test2 />} />
+      <Route exact path='/' element={<Login />} />
+      <Route path='/table' element={<Table />} />
             
       </Routes>
 
