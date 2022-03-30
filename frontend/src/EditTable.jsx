be6ffed0d70bce2d443487e5089a839fab84a69b
Anish Pokhrel <anishpokhrel2002@gmail.com>
2022-03-30 17:03:41 -0600

    Entry table edit functions

---
+++ frontend/src/EditTable.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const EditTable = ({
+  editFormData,
+  handleEditFormChange,
+  handleCancelClick,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter name"
+          name="entryId"
+          value={editFormData.entryId}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter name"
+          name="personName"
+          value={editFormData.personName}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter location"
+          name="location"
+          value={editFormData.location}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="date"
+          required="required"
+          placeholder="Enter date"
+          name="date"
+          value={editFormData.date}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditTable;
